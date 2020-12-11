c8ef21b54f7f3734499633b40afb528c162818b3
Prashant Keshwad <prashantkeshwad@gmail.com>
2020-12-11 19:14:39 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -122,6 +122,9 @@ class App extends React.Component {
   }
 
   render(){
+
+    console.log(this.response);
+
     const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
